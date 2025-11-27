44a467f038c8785aa6ab3fb20621e5b179bdab05
Abu Kalam <akwebdev69@gmail.com>
2025-11-27 06:42:47 +0600

    dskjdksd

--- src/Pages/Account.jsx
+++ src/Pages/Account.jsx
@@ -88,7 +88,7 @@ const Account = () => {
                             whileTap={{ scale: 0.95 }}
                             onClick={() => navigate("/login")}
                             style={{ backgroundColor: primaryColor }}
-                            class name="px-8 py-4 text-white font-semibold rounded-xl shadow-xl hover:brightness-110 transition-all flex items-center justify-center gap-3"
+                            className="px-8 py-4 text-white font-semibold rounded-xl shadow-xl hover:brightness-110 transition-all flex items-center justify-center gap-3"
                         >
                             <Mail className="w-5 h-5" />
                             Sign In
